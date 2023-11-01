b4dc828a2b647f30b0cff2695ad9ec9d79b5b4ed
aicul313 <lucia_0313@naver.com>
2023-11-01 13:56:04 +0900

    initial setting

---
+++ welfareservice-FE/src/styles/Theme.jsx
@@ -0,0 +1,24 @@
+export const colors = {
+  mainColor: "#F9A237",
+  impactColor: "#56310F",
+  disabledColor: "#A6896E",
+  grayColor: "#EEEEEE",
+  placeHolderColor: "#767676",
+  whiteColor: "#FFFFFF",
+  blackColor: "#000000",
+  alertColor: "#FF0000",
+};
+
+export const fontSize = {
+  xlarge: "2rem", // 32px
+  large: "1.25rem", // 20px
+  medium: "1rem", // 16px
+  small: "0.75rem", // 12px
+};
+
+const Theme = {
+  colors,
+  fontSize,
+};
+
+export default Theme;
