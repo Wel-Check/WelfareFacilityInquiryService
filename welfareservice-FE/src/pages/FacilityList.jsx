f60b48de48bbe406b43571249bcc921e8d630fee
aicul313 <lucia_0313@naver.com>
2023-11-10 00:17:20 +0900

    ✨ 서버에서 데이터 가져오기

--- welfareservice-FE/src/pages/FacilityList.jsx
+++ welfareservice-FE/src/pages/FacilityList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router";
+import axios from "axios";
 import styled from "styled-components";
 import Navbar from "../components/Navbar/Navbar";
 import ListHeader from "../components/FacilityList/ListHeader";
@@ -12,13 +13,30 @@ export const Title = styled.h3`
 
 const FacilityList = () => {
   let { title } = useParams();
+
+  // 서버에서 데이터 가져오는 로직
+  const [facilities, setFacilities] = React.useState([]);
+
+  React.useEffect(() => {
+    // 데이터 가져오기
+    axios.get("http://localhost:8080/welfare/facilitie/facilities")
+      .then(response => {
+        // 성공적으로 데이터를 가져왔다면, 이를 facilities 상태에 저장합니다.
+        setFacilities(response.data);
+      })
+      .catch(error => {
+        console.error('Failed to fetch facilities:', error);
+      });
+  }, []);
   
   return (
       <>
         <Navbar />
         <Title>{title}</Title>
         <ListHeader />
-        <ListItem />
+        {facilities.map((facility) => (
+        <ListItem facility={facility} key={facility.fno} />
+      ))}
       </>
   );
 };
